37b3547fad3cb52f6a298c80e5915ffa564b60d6
Phil <pbb59@cornell.edu>
2019-11-09 14:05:52 -0500

    print instruction opcodes

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -3,15 +3,32 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+
 using namespace llvm;
 
 namespace {
+  // TODO potentially want a LoopPass b/c want to trace across within a loop nest and not outside
   struct SkeletonPass : public FunctionPass {
     static char ID;
     SkeletonPass() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
       errs() << "I saw a function called " << F.getName() << "!\n";
+      for (auto &B : F) {
+        for (auto &I : B) {
+          /*if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+            errs() << "I saw an op called " << op->getName() << "!\n";
+          }*/
+          errs() << "Instruction " << I.getOpcodeName();
+        }
+      }
+
+
+      // whether code was modified or not
       return false;
     }
   };
